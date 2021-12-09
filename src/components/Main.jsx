25a358999eaf540ab057282de1ad1bbb44c608f0
ShingoTennichi <sngsng.ttt@gmail.com>
2021-12-09 04:53:56 -0800

    Create react app

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+    return (
+        <>
+            <div>
+                <h1>GREAT</h1>
+            </div>
+        </>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <>
+            <nav className='Navbar-wrapper'>
+                <div className='Navbar-container'>
+                </div>
+            </nav>
+        </>
+    )
+}
+export default Navbar
